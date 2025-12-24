c150a42855662bb96489c151462c0977b09a250c
Pratham Kumar Taheem <pratham@Prathams-MacBook-Air.local>
2025-12-24 21:34:04 +0530

    First file

---
+++ First.java
@@ -0,0 +1,10 @@
+public class First {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+        System.out.println("bye world");
+         System.out.println("bye world");
+          System.out.println("bye world");
+           System.out.println("bye world");
+    }
+}
