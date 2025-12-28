374367672a28a079d2660d4346377fef92064ad1
jsakshi02 <sakshi73175@gmail.com>
2025-12-28 21:20:22 +0530

    dotNet web-Api module added.

---
+++ Employee/Model/Entities/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace Employee.Model.Entities
+{
+    public class Employee
+    {
+        
+        public int Id { get; set; }
+        public required String Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+
+    }
+}
