1dc03ad6db4403906cc0d2f7a33e86bf1b92beb7
Amogh Parmar <amoghparmar30@gmail.com>
2026-06-16 11:19:41 +0530

    changes done

--- Credentails.java
+++ Credentails.java
@@ -1,7 +1,8 @@
 public class Credentails {
     private String password = "helo12223";
     private String user = "1231";
+
     public static void main(String[] args) {
-        System.out.println("Change to reflect tag in repo remote");
+        System.out.println("Change to reflect tag in repo remotee");
     }
 }
